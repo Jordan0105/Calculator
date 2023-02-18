6efcfb4161cbd23de8b1c94e49b07e2c397c7e15
Jordan0105 <102000003+Jordan0105@users.noreply.github.com>
2023-02-18 15:00:06 -0600

    Some Fixes
    Some bugs are fixed

--- index.js
+++ index.js
@@ -1,293 +1,255 @@
-let result = undefined;
-let clicks = 0;
-
 let mathOperation = 0, numPressed = "", click = 0;
 
 const output = document.getElementById("result");
 const element = document.getElementById("alertMessage");
 const alertMessage = window.getComputedStyle(element);
 
+const numberButtons = document.getElementsByClassName("numberButtons");
+const operators = document.getElementsByClassName("operators");
 
+let option = undefined;
+let savedValue = null;
 let num1 = null, num2 = null;
+
+//All clear function
 
 function ac() {
 
   output.innerHTML = "";
-
   num1 = null;
   num2 = null;
+  savedValue = null;
   numPressed = "";
+  option = 0;
 
   element.style.display = "none";
 
   click = 0;
 }
 
-function one() {
 
-  if (click < 12) {
-    output.innerHTML = numPressed;
-    numPressed += "1";
-  }
-
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-
-}
-
-function two() {
-
-  if (click < 12) {
-    numPressed += "2";
-    output.innerHTML = numPressed;
-  }
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function three() {
-
-
-  if (click < 12) {
-    numPressed += "3";
-    output.innerHTML = numPressed;
-  }
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function four() {
-
-
-  if (click < 12) {
-    numPressed += "4";
-    output.innerHTML = numPressed;
-  }
-
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function five() {
-
-  if (click < 12) {
-    numPressed += "5";
-    output.innerHTML = numPressed;
-  }
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function six() {
-
-  if (click < 12) {
-    numPressed += "6";
-    output.innerHTML = numPressed;
-  }
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function seven() {
-
-  if (click < 12) {
-    numPressed += "7";
-    output.innerHTML = numPressed;
-  }
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function eigth() {
-
-  if (click < 12) {
-    numPressed += "8";
-    output.innerHTML = numPressed;
-  }
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function nine() {
-
-  if (click < 12) {
-    numPressed += "9";
-    output.innerHTML = numPressed;
-  }
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function zero() {
-
-  if (click < 12) {
-    numPressed += "0";
-    output.innerHTML = numPressed;
-  }
-  else if (click >= 12 && alertMessage.display === "none")
-    element.style.display = "block";
-
-  click++;
-}
-
-function add() {
-
-  num1 = parseInt(numPressed);
-  let check = isNaN(num1);
-
-  if (check != true && num1 != null) {
-
-    num2 = num1;
-    numPressed = "";
-    mathOperation = 1;
-    output.innerHTML = "+";
-    element.style.display = "none";
-    click = 0;
-  }
-
-
-}
-
-function sustr() {
-
-  num1 = parseInt(numPressed);
-  let check = isNaN(num1);
-  if (check != true && num1 != null) {
-
-    num2 = num1;
-    numPressed = "";
-    mathOperation = 2;
-    element.style.display = "none";
-    output.innerHTML = "-";
-    click = 0;
-  }
-}
-
-function mult() {
-  num1 = parseInt(numPressed);
-  let check = isNaN(num1);
-  if (check != true && num1 != null) {
-
-    num2 = num1;
-    numPressed = "";
-    mathOperation = 3;
-    element.style.display = "none";
-    output.innerHTML = "x";
-    click = 0;
-  }
-}
-
-function divition() {
-  num1 = parseInt(numPressed);
-  let check = isNaN(num1);
-  if (check != true && num1 != null) {
-
-    num2 = num1;
-    numPressed = "";
-    mathOperation = 4;
-    output.innerHTML = "÷";
-    element.style.display = "none";
-    click = 0;
-
-  }
-}
-
-
+// If the number of digits exceeds the output
 
 function amountDigits(result) {
+
   let resultString = result.toString();
 
-  if (resultString.length > 13) {
+  if (resultString.length > 15 && mathOperation == 4) {
 
-    //Convert the result in exponential notation
+    resultString = resultString.slice(0, 15);
 
-    resultString = result.toExponential(5);
   }
+  else if (resultString.length > 15 && !(mathOperation == 4)) {
+    resultString = result.toExponential(9);
+  }
+
+  savedValue = result;
   output.innerHTML = resultString;
 }
 
-function equal() {
+//Equal Button Function
 
-  //Addition
+const equalResult = () => {
 
+  element.style.display = "none";
   click = 0;
 
-  if (mathOperation == 1) {
+  switch (mathOperation) {
+    case 1:
+      num1 = parseInt(numPressed);
+      if (isNaN(num1) === true || isNaN(num2) === true) {
+        result = "Syntax Error";
+        output.innerHTML = result;
+      }
+      else {
+        result = num1 + num2;
+        amountDigits(result);
+      }
+      break;
 
-    num1 = parseInt(numPressed);
-    if (num1 != -1 && num2 != null) {
+    case 2:
+      num1 = parseInt(numPressed);
+      if (isNaN(num1) === true || isNaN(num2) === true) {
+        result = "Syntax Error";
+        output.innerHTML = result;
+      }
 
-      result = num1 + num2;
-      amountDigits(result);
+      else {
+        result = num2 - num1;
+        amountDigits(result);
+      }
+      break;
 
-    }
+    case 3:
+      num1 = parseInt(numPressed);
+      if (isNaN(num1) === true || isNaN(num2) === true) {
+        result = "Syntax Error";
+        output.innerHTML = result;
+      }
+      else {
 
+        result = num2 * num1;
+        amountDigits(result);
+      }
+      break;
 
+    case 4:
+      num1 = parseInt(numPressed);
+
+      if (isNaN(num1) === true || isNaN(num2) === true) {
+        result = "Syntax Error";
+        output.innerHTML = result;
+      }
+      else if (num1 === 0) {
+        result = "Divition by 0 not allowed";
+        output.innerHTML = result;
+      }
+      else {
+        result = num2 / num1;
+        amountDigits(result);
+      }
+      break;
+
+    default:
+      return;
   }
 
-  //Substraction
+}
 
-  else if (mathOperation == 2) {
-    num1 = parseInt(numPressed);
-    if (num1 != null && num2 != null) {
+const numberPressed = iterator => {
 
-      result = num2 - num1;
-      amountDigits(result);
-
-    }
+  if (click === 0 && option === "=") {
+    ac();
   }
 
-  //Multiplication
+  option = (iterator.innerHTML).trim();
+  switch (option) {
+    case '0':
+      clickedButtons('0');
+      break;
+    case '1':
+      clickedButtons('1');
+      break;
+    case '2':
+      clickedButtons('2');
+      break;
+    case '3':
+      clickedButtons('3');
+      break;
+    case '4':
+      clickedButtons('4');
+      break;
+    case '5':
+      clickedButtons('5');
+      break;
+    case '6':
+      clickedButtons('6');
+      break;
+    case '7':
+      clickedButtons('7');
+      break;
+    case '8':
+      clickedButtons('8');
+      break;
+    case '9':
+      clickedButtons('9');
+      break;
+    default:
+      return;
+  }
+}
 
-  else if (mathOperation == 3) {
+// This function does the different mathematical operations.
+
+const toDoOperation = (numMathOp, sign) => {
+
+  if (savedValue === null) {
+
     num1 = parseInt(numPressed);
-    if (num1 != null && num2 != null) {
 
-      result = num1 * num2;
-      amountDigits(result);
+    if (num1 != null) {
 
-
+      num2 = num1;
+      mathOperation = numMathOp;
+      numPressed = "";
+      output.innerHTML = sign;
+      element.style.display = "none";
+      click = 0;
     }
   }
-
-  //Divition
-
-  else if (mathOperation == 4) {
-
-    num1 = parseInt(numPressed);
-
-    if (num1 != 0 && (num1 != null && num2 != null)) {
-
-      result = num2 / num1;
-      amountDigits(result);
-
-
-    } else if (num1 == 0) {
-
-      result = "Divition by 0 not allowed";
-      output.innerHTML = result;
-      output.style.fontSize = "90%";
-
-    }
+  else {
+    num2 = savedValue;
+    mathOperation = numMathOp;
+    numPressed = "";
+    output.innerHTML = sign;
+    element.style.display = "none";
+    click = 0;
   }
+}
+
+const operatorPressed = iterator => {
+  option = (iterator.innerHTML).trim();
+
+  switch (option) {
+
+    case '+':
+      toDoOperation(1, option);
+      break;
+
+    //TODO: If output.innerHTML == "" then - is part of the number
+
+    case '-':
+
+      toDoOperation(2, option);
+      break;
+
+    case '×':
+
+      toDoOperation(3, option)
+      break;
 
 
+    case '÷':
+      toDoOperation(4, option);
+      break;
+
+    case '=':
+      equalResult();
+      break;
+
+    case 'AC':
+      ac();
+      break;
+
+  }
+}
+
+//This function detects if the user has exceeded 
+//the total number of digits per operation
+
+const clickedButtons = num => {
+
+
+  if (click < 12) {
+    numPressed += num;
+    output.innerHTML = numPressed;
+  }
+  else if (click >= 12 && alertMessage.display === "none")
+    element.style.display = "block";
+
+  click++;
+}
+
+
+//Button Event Listeners
+for (const iterator of numberButtons) {
+  iterator.addEventListener("click", () => {
+    numberPressed(iterator)
+  });
+}
+
+for (const iterator of operators) {
+  iterator.addEventListener("click", () => {
+    operatorPressed(iterator)
+  });
 }
